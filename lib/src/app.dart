c83917a97852a0d050883ea947e142bf31b771c4
Marijn <25235249+2zqa@users.noreply.github.com>
2024-05-31 15:30:52 +0200

    Use GoRouter

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,17 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:vanholst/src/features/products_list/products_list_screen.dart';
 import 'package:vanholst/src/localization/string_hardcoded.dart';
+
+// GoRouter configuration
+final _router = GoRouter(
+  initialLocation: '/',
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => const ProductsListScreen(),
+    ),
+  ],
+);
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       restorationScopeId: 'app',
-      // * The home page of the app
-      home: const ProductsListScreen(),
+      routerConfig: _router,
       onGenerateTitle: (BuildContext context) => 'My Shop'.hardcoded,
       theme: ThemeData(
         primarySwatch: Colors.grey,
