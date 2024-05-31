d33b6caff7e94e936365dafbe82cfb515186e03b
Marijn <25235249+2zqa@users.noreply.github.com>
2024-05-31 16:52:11 +0200

    add login route

--- lib/src/routing/app_router.dart
+++ lib/src/routing/app_router.dart
@@ -1,8 +1,11 @@
 // GoRouter configuration
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:vanholst/src/features/account/account_screen.dart';
 import 'package:vanholst/src/features/products_list/products_list_screen.dart';
+import 'package:vanholst/src/features/sign_in/email_password_sign_in_screen.dart';
+import 'package:vanholst/src/features/sign_in/email_password_sign_in_state.dart';
 
 final goRouter = GoRouter(
   initialLocation: '/',
@@ -15,7 +18,17 @@ final goRouter = GoRouter(
         GoRoute(
           path: 'account',
           builder: (context, state) => const AccountScreen(),
-        )
+        ),
+        GoRoute(
+          path: 'login',
+          pageBuilder: (context, state) => MaterialPage(
+            key: state.pageKey,
+            fullscreenDialog: true,
+            child: const EmailPasswordSignInScreen(
+              formType: EmailPasswordSignInFormType.signIn,
+            ),
+          ),
+        ),
       ],
     )
   ],
