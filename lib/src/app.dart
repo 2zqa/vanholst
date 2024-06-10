3eaa9bd1d33691097fc0089e5c29514c9619c873
Marijn <25235249+2zqa@users.noreply.github.com>
2024-06-10 22:13:15 +0200

    Enable redirects based on auth

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:vanholst/src/localization/string_hardcoded.dart';
 import 'package:vanholst/src/routing/app_router.dart';
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp.router(
-      routerConfig: goRouter,
+      routerConfig: ref.watch(goRouterProvider),
       debugShowCheckedModeBanner: false,
       restorationScopeId: 'app',
       onGenerateTitle: (BuildContext context) => 'My Shop'.hardcoded,
--- lib/src/routing/app_router.dart
+++ lib/src/routing/app_router.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:vanholst/src/features/authentication/data/auth_repository.dart';
 import 'package:vanholst/src/features/authentication/presentation/account/account_screen.dart';
 import 'package:vanholst/src/features/authentication/presentation/sign_in/sign_in_screen.dart';
 import 'package:vanholst/src/features/logbook/presentation/product_screen/product_screen.dart';
@@ -13,41 +15,55 @@ enum AppRoute {
   signIn,
 }
 
-final goRouter = GoRouter(
-  initialLocation: '/',
-  debugLogDiagnostics: false,
-  routes: [
-    GoRoute(
-      path: '/',
-      name: AppRoute.home.name,
-      builder: (context, state) => const ProductsListScreen(),
-      routes: [
-        GoRoute(
-          path: 'product/:id',
-          name: AppRoute.product.name,
-          builder: (context, state) {
-            final productId = state.pathParameters['id']!;
-            return ProductScreen(productId: productId);
-          },
-        ),
-        GoRoute(
-          path: 'account',
-          name: AppRoute.account.name,
-          pageBuilder: (context, state) => const MaterialPage(
-            fullscreenDialog: true,
-            child: AccountScreen(),
+final goRouterProvider = Provider<GoRouter>((ref) {
+  final authRepository = ref.watch(authRepositoryProvider);
+  return GoRouter(
+    initialLocation: '/',
+    debugLogDiagnostics: false,
+    redirect: (_, state) {
+      final isLoggedIn = authRepository.currentUser != null;
+      final path = state.uri.path;
+      if (isLoggedIn && path == '/signIn') {
+        return '/';
+      }
+      if (!isLoggedIn && path == '/account') {
+        return '/';
+      }
+      return null;
+    },
+    routes: [
+      GoRoute(
+        path: '/',
+        name: AppRoute.home.name,
+        builder: (context, state) => const ProductsListScreen(),
+        routes: [
+          GoRoute(
+            path: 'product/:id',
+            name: AppRoute.product.name,
+            builder: (context, state) {
+              final productId = state.pathParameters['id']!;
+              return ProductScreen(productId: productId);
+            },
           ),
-        ),
-        GoRoute(
-          path: 'signIn',
-          name: AppRoute.signIn.name,
-          pageBuilder: (context, state) => const MaterialPage(
-            fullscreenDialog: true,
-            child: SignInScreen(),
+          GoRoute(
+            path: 'account',
+            name: AppRoute.account.name,
+            pageBuilder: (context, state) => const MaterialPage(
+              fullscreenDialog: true,
+              child: AccountScreen(),
+            ),
           ),
-        ),
-      ],
-    ),
-  ],
-  errorBuilder: (context, state) => const NotFoundScreen(),
-);
+          GoRoute(
+            path: 'signIn',
+            name: AppRoute.signIn.name,
+            pageBuilder: (context, state) => const MaterialPage(
+              fullscreenDialog: true,
+              child: SignInScreen(),
+            ),
+          ),
+        ],
+      ),
+    ],
+    errorBuilder: (context, state) => const NotFoundScreen(),
+  );
+});
