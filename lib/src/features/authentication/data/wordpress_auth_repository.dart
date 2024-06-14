8f44eb3241e7f62fc92b7ca791ebf7751c59d230
Marijn <25235249+2zqa@users.noreply.github.com>
2024-06-14 11:30:45 +0200

    Fake the `sbjs_current_add` and `sbjs_first_add` values more realistically

--- lib/src/features/authentication/data/wordpress_auth_repository.dart
+++ lib/src/features/authentication/data/wordpress_auth_repository.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
+import 'package:intl/intl.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:sembast/sembast.dart';
 import 'package:sembast/sembast_io.dart';
@@ -141,12 +142,15 @@ class WordpressAuthRepository implements AuthRepository {
   }
 
   Future<http.Response> _getVanHolstLoginResponse() async {
+    final now = DateTime.now().subtract(const Duration(seconds: 5));
+    final formattedTime = DateFormat('yyyy-MM-dd HH:mm:ss').format(now);
+    final encodedTime = Uri.encodeComponent(formattedTime);
     final headers = {
       'accept':
           'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
       'accept-language': 'nl-NL,nl;q=0.9,en-US;q=0.8,en;q=0.7',
       'cookie':
-          'sbjs_migrations=1418474375998%3D1; sbjs_current_add=fd%3D2024-06-14%2008%3A48%3A10%7C%7C%7Cep%3Dhttps%3A%2F%2Fwww.vanholstcoaching.nl%2F%7C%7C%7Crf%3Dhttps%3A%2F%2Fwww.google.com%2F; sbjs_first_add=fd%3D2024-06-14%2008%3A48%3A10%7C%7C%7Cep%3Dhttps%3A%2F%2Fwww.vanholstcoaching.nl%2F%7C%7C%7Crf%3Dhttps%3A%2F%2Fwww.google.com%2F; sbjs_current=typ%3Dorganic%7C%7C%7Csrc%3Dgoogle%7C%7C%7Cmdm%3Dorganic%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29; sbjs_first=typ%3Dorganic%7C%7C%7Csrc%3Dgoogle%7C%7C%7Cmdm%3Dorganic%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29; sbjs_udata=vst%3D1%7C%7C%7Cuip%3D%28none%29%7C%7C%7Cuag%3DMozilla%2F5.0%20%28X11%3B%20Linux%20x86_64%29%20AppleWebKit%2F537.36%20%28KHTML%2C%20like%20Gecko%29%20Chrome%2F125.0.0.0%20Safari%2F537.36; sbjs_session=pgs%3D4%7C%7C%7Ccpg%3Dhttps%3A%2F%2Fwww.vanholstcoaching.nl%2F',
+          'sbjs_migrations=1418474375998%3D1; sbjs_current_add=fd%3D$encodedTime%7C%7C%7Cep%3Dhttps%3A%2F%2Fwww.vanholstcoaching.nl%2F%7C%7C%7Crf%3Dhttps%3A%2F%2Fwww.google.com%2F; sbjs_first_add=fd%3D$encodedTime%7C%7C%7Cep%3Dhttps%3A%2F%2Fwww.vanholstcoaching.nl%2F%7C%7C%7Crf%3Dhttps%3A%2F%2Fwww.google.com%2F; sbjs_current=typ%3Dorganic%7C%7C%7Csrc%3Dgoogle%7C%7C%7Cmdm%3Dorganic%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29; sbjs_first=typ%3Dorganic%7C%7C%7Csrc%3Dgoogle%7C%7C%7Cmdm%3Dorganic%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29; sbjs_udata=vst%3D1%7C%7C%7Cuip%3D%28none%29%7C%7C%7Cuag%3DMozilla%2F5.0%20%28X11%3B%20Linux%20x86_64%29%20AppleWebKit%2F537.36%20%28KHTML%2C%20like%20Gecko%29%20Chrome%2F125.0.0.0%20Safari%2F537.36; sbjs_session=pgs%3D4%7C%7C%7Ccpg%3Dhttps%3A%2F%2Fwww.vanholstcoaching.nl%2F',
       'priority': 'u=0, i',
       'referer': 'https://www.vanholstcoaching.nl/',
       'sec-ch-ua':
