7d5fdfcebd6a7e6e3e9db750d2bdf4ee9976f6bb
Marijn <25235249+2zqa@users.noreply.github.com>
2024-09-21 21:00:58 +0200

    Add LinkText common widget

---
+++ lib/src/common_widgets/link_text.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class LinkText extends StatelessWidget {
+  final String text;
+  final String? uriText;
+  final Uri uri;
+  final TextStyle? style;
+  final TextStyle? urlStyle;
+  final void Function()? onFailLaunchUri;
+
+  /// Creates a paragraph of text, where the first instance of [uriText] is
+  /// replaced with a link to [uri].
+  const LinkText(
+    this.text, {
+    super.key,
+    this.uriText,
+    required this.uri,
+    this.onFailLaunchUri,
+    this.style,
+    this.urlStyle,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final realUriText = uriText ?? uri.toString();
+    final beforeUriText = text.substring(0, text.indexOf(realUriText));
+    final afterUriText =
+        text.substring(text.indexOf(realUriText) + realUriText.length);
+
+    final DefaultTextStyle defaultTextStyle = DefaultTextStyle.of(context);
+    final TextStyle effectiveTextStyle = style ?? defaultTextStyle.style;
+
+    return RichText(
+      text: TextSpan(
+        style: effectiveTextStyle,
+        children: [
+          TextSpan(text: beforeUriText),
+          TextSpan(
+            text: realUriText,
+            style: urlStyle ??
+                effectiveTextStyle.copyWith(
+                  color: Theme.of(context).colorScheme.secondary,
+                  decoration: TextDecoration.underline,
+                ),
+            recognizer: TapGestureRecognizer()
+              ..onTap = () async {
+                final canLaunch = await canLaunchUrl(uri);
+                if (!canLaunch) {
+                  onFailLaunchUri?.call();
+                  return;
+                }
+                await launchUrl(uri);
+              },
+          ),
+          TextSpan(text: afterUriText),
+        ],
+      ),
+    );
+  }
+}
