d5a214a2504ddd4782a4e47a2392b728256f48e6
Marijn <25235249+2zqa@users.noreply.github.com>
2024-09-26 23:38:55 +0200

    Fix logbook entry not showing changes after edit

--- lib/src/features/logbook/presentation/logbook_entry_screen/logbook_entry_edit_screen_controller.dart
+++ lib/src/features/logbook/presentation/logbook_entry_screen/logbook_entry_edit_screen_controller.dart
@@ -3,15 +3,19 @@ import 'package:vanholst/src/features/logbook/data/logbook_repository.dart';
 import 'package:vanholst/src/features/logbook/domain/logbook_entry.dart';
 
 class LogbookEntryEditScreenController extends StateNotifier<AsyncValue<void>> {
-  LogbookEntryEditScreenController({required this.logbookNotifier})
-      : super(const AsyncValue.data(null));
+  LogbookEntryEditScreenController({
+    required this.logbookNotifier,
+    required this.ref,
+  }) : super(const AsyncValue.data(null));
 
   final LogbookNotifier logbookNotifier;
+  final Ref ref;
 
   Future<bool> save(LogbookEntry entry) async {
     state = const AsyncValue.loading();
     state = await AsyncValue.guard(() async {
       await logbookNotifier.updateLogbookEntry(entry);
+      ref.invalidate(logbookEntryProvider(entry.id));
     });
     return state.hasError == false;
   }
@@ -21,5 +25,5 @@ final logbookEntryEditScreenControllerProvider =
     StateNotifierProvider<LogbookEntryEditScreenController, AsyncValue<void>>(
         (ref) {
   return LogbookEntryEditScreenController(
-      logbookNotifier: ref.watch(logbookNotifierProvider.notifier));
+      logbookNotifier: ref.watch(logbookNotifierProvider.notifier), ref: ref);
 });
