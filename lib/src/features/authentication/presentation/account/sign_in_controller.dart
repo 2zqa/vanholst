674ee70cf13def3650d18a63aa2c30f0019b954a
Marijn <25235249+2zqa@users.noreply.github.com>
2024-10-01 21:12:39 +0200

    Remove debugPrint message

--- lib/src/features/authentication/presentation/account/sign_in_controller.dart
+++ lib/src/features/authentication/presentation/account/sign_in_controller.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:vanholst/src/features/authentication/data/auth_repository.dart';
 import 'package:vanholst/src/features/authentication/presentation/sign_in/email_password_sign_in_state.dart';
@@ -11,7 +10,6 @@ class SignInController extends StateNotifier<SignInState> {
 
   void togglePasswordVisibility() {
     state = state.copyWith(isPasswordHidden: !state.isPasswordHidden);
-    debugPrint('Password visibility is now ${state.isPasswordHidden}');
   }
 
   Future<bool> submit(String email, String password) async {
